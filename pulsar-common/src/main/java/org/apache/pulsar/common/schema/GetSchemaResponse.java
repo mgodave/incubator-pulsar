f72b9ad1b12049360b3e1fd837866098c08076f4
Dave Rusek <dave@streaml.io>
2018-05-01 10:17:52 -0600

    revert errant commit

--- pulsar-common/src/main/java/org/apache/pulsar/common/schema/GetSchemaResponse.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/schema/GetSchemaResponse.java
@@ -21,11 +21,9 @@ package org.apache.pulsar.common.schema;
 import java.util.Map;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@NoArgsConstructor
 public class GetSchemaResponse {
     private SchemaVersion version;
     private SchemaType type;
